e49ed0f13f7bccd7ebc889d5156e3a733ccfa41b
LAPTOP-KFNK5QPH\uurus <moais611@gmail.com>
2025-12-07 19:07:16 +0300

    add abstract resource

---
+++ Abstracts/AbstractResource.cs
@@ -0,0 +1,20 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace XCommon.Abstracts
+{
+    public abstract class AbstractResource : ScriptableObject
+    {
+#if UNITY_EDITOR
+        protected void OnValidate()
+        {
+            EditorUtility.SetDirty(this);
+            
+            EditorApplication.delayCall += () =>
+            {
+                AssetDatabase.SaveAssets();
+            };
+        }
+#endif
+    }
+}
